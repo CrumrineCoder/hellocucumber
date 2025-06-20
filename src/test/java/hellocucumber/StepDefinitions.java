c8dfa836a0d8f1e574230fd882104a81e6934a41
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-06-20 13:03:00 -0500

    Variables

--- src/test/java/hellocucumber/StepDefinitions.java
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -15,9 +15,9 @@ public class StepDefinitions {
     private String today;
     private String actualAnswer;
 
-    @Given("today is Sunday")
-    public void today_is_Sunday() {
-        today = "Sunday";
+    @Given("today is {string}")
+    public void today_is(String today) {
+        this.today = today;
     }
 
     @When("I ask whether it's Friday yet")
@@ -28,10 +28,5 @@ public class StepDefinitions {
     @Then("I should be told {string}")
     public void i_should_be_told(String expectedAnswer) {
         assertThat(actualAnswer).isEqualTo(expectedAnswer);
-    }
-
-    @Given("today is Friday")
-    public void today_is_Friday() {
-        today = "Friday";
     }
 }
